3c4b645df06d53ce1737175b31b5eeaa91fa98e5
Christian Kaestner <kaestner@cs.cmu.edu>
2020-05-28 18:51:53 -0400

    fix reveal-md path issues

--- lectures/_assets/loadmymarkdown.js
+++ lectures/_assets/loadmymarkdown.js
@@ -1,8 +1,6 @@
 //add own markdown extensions
-console.log("loading markdown extensions")
 const pathMD = deps[1].src.replace("plugin/markdown/markdown.js","_assets/_assets/plugin/mymarkdown.js")
 const pathFitty = deps[1].src.replace("plugin/markdown/markdown.js","_assets/_assets/plugin/fitty.min.js")
 deps.splice(2,0,{src:pathMD})
 deps.push({src:pathFitty})
-console.log(pathMD)
 console.log(deps)
