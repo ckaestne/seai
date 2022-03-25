ed44d3d40ad8710e8112f2e216af26dd7cf602a0
tashee7 <t@Ts-MacBook-Air.local>
2022-03-24 20:29:08 -0400

    Add Recitation 8 material

---
+++ recitations/08_monitoring/kafka-monitor.py
@@ -0,0 +1,39 @@
+from kafka import KafkaConsumer
+from prometheus_client import Counter, Histogram, start_http_server
+
+start_http_server(8765)
+
+#Counter, Gauge, Histogram, Summaries
+REQUEST_COUNT = Counter(
+    'request_count', 'Recommendation Request Count',
+    ['http_status']
+)
+
+REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency')
+
+
+def main():
+    consumer = KafkaConsumer(
+        '<<Topic>>', #topic here
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest',
+        group_id='<<GROUP_ID>>', #group ID here
+        enable_auto_commit=True,
+        auto_commit_interval_ms=1000
+    )
+
+    for message in consumer:
+        event = message.value.decode('utf-8')
+        values = event.split(',')
+
+        if 'recommendation request' in values[2]:
+            print(values)
+            status = values[3].strip().split(" ")[1]
+            REQUEST_COUNT.labels(status).inc()
+
+            time_taken = float(values[-1].strip().split(" ")[0])
+            REQUEST_LATENCY.observe(time_taken / 1000)
+
+
+if __name__ == "__main__":
+    main()
